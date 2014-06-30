7ed52b8edce5385da3a0e487a4c86c91298f975b
Mike Engel <mike@mike-engel.com>
2014-06-29 23:53:57 -0600

    Adding pagination

--- app.rb
+++ app.rb
@@ -14,43 +14,57 @@ include Mongo
 db = MongoClient.new(ENV["DATABASE_URL"] || "localhost", 27017).db("bkmrkd")
 bookmarks = db.collection('bookmarks')
 
+links_per_page = 25.0
 
 helpers do
 
 end
 
+bookmarks.remove
+
+30.times do |n|
+    bookmarks.insert(id: n, title: "Testing #{n}", formatted_url: 'http://www.google.com', date_added: Time.now)
+end
+
 get '/' do
-  @bookmarks = bookmarks.find.to_a.reverse!
-  if @bookmarks
-    puts 'found some bookmarks'
-  else
-    puts 'couldn\'t find any bookmarks'
-  end
-  haml :index
+    if params[:page]
+        @page_number = params[:page].to_i
+    else
+        @page_number = 1
+    end
+
+    @bookmark_pages = (bookmarks.count / links_per_page).ceil
+    @bookmarks = bookmarks.find.sort(id: :desc).skip(links_per_page * (@page_number - 1)).limit(links_per_page).to_a
+
+    puts "number of bookmarks: #{bookmarks.count}"
+    puts "page number: #{@page_number}"
+    puts "number of pages: #{@bookmark_pages}"
+
+    haml :index
 end
 
 get '/colophon' do
-  haml :colophon
+    haml :colophon
 end
 
 get '/delete/:token' do
-  @id = params[:token].to_i(10)
-  bookmarks.remove("id" => @id)
-  flash.next[:success] = 'Bookmarklet deleted successfully'
-  redirect '/'
+    @id = params[:token].to_i
+    bookmarks.remove(id: @id)
+    flash.next[:success] = 'Bookmarklet deleted successfully'
+    redirect '/'
 end
 
 get '/add' do
-  @title = params['title']
-  @url = params['url']
-  @callback = params['callback']
-  @action = params['action']
-  @id = bookmarks.count + 1
-  @formatted_url = @url.split('/')[2]
+    @title = params['title']
+    @url = params['url']
+    @callback = params['callback']
+    @action = params['action']
+    @id = bookmarks.count + 1
+    @formatted_url = @url.split('/')[2]
 
-  @bookmark = bookmarks.insert("id" => @id, "title" => @title, "url" => @url, "formatted_url" => @formatted_url, "date_added" => Time.now)
+    @bookmark = bookmarks.insert(id: @id, title: @title, url: @url, formatted_url: @formatted_url, date_added: Time.now)
 
-  if @action === 'close'
-    return 'window.close();'
-  end
+    if @action === 'close'
+        return 'window.close();'
+    end
 end
