7d1324c9eccc6a62dcd0632c0b186ce597988291
Mike Engel <mike@mike-engel.com>
2016-09-27 23:20:14 -0600

    Add docker-compose, nvmrc, and start config file

---
+++ config/index.js
@@ -0,0 +1,10 @@
+const env = process.env
+
+module.exports = () => ({
+  db: {
+    host: env.DB_HOST || 'localhost',
+    port: env.DB_PORT || 28015
+  },
+  env: env.NODE_ENV || 'development',
+  port: env.PORT || 3000
+})
