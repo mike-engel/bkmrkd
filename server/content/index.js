4b44235aaf9baf9730d3e9e34f8e1b85362e3f09
Mike Engel <mike@mike-engel.com>
2016-11-15 22:41:15 -0700

    Uglify elm file for production

--- server/content/index.js
+++ server/content/index.js
@@ -1,10 +1,13 @@
+const { env } = require('config')
 const { Router } = require('express')
 
 const contentRouter = Router()
 
 contentRouter.route(/^\/$|^\/colophon$/i)
   .get((req, res) => {
-    return res.status(200).render('index')
+    return res.status(200).render('index', {
+      env
+    })
   })
 
 module.exports = contentRouter
