abc1d80bc79326346c439d925387407c9bb934d7
Mike Engel <mike@mike-engel.com>
2017-02-11 13:15:17 -0700

    Remove old source directory/code

--- src/js/helpers/snippet.js
+++
@@ -1,6 +0,0 @@
-import config from '../../../helpers/config'
-
-const env = process.env.NODE_ENV || 'development'
-const snippetURL = `javascript:function bkmrk(){var a=document.createElement("script"),b=window.location,c=document.title,d="${typeof window !== 'undefined' ? window.location.origin : config[env].snippetHost}/api/create?title="+c+"&url="+encodeURIComponent(b.href);try{/https/i.test(b.protocol)?window.open(d+"&action=close","_blank","width=100,height=100"):(document.title="Bookmarking "+c,a.setAttribute("src",d),a.setAttribute("async",""),document.body.appendChild(a),document.title=c)}catch(e){alert("Please wait until the page has loaded.")}}bkmrk();void 0;`
-
-export default snippetURL
