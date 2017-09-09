eb513625e80311d5652f8cb7211fdf479f829383
Mike Engel <mike@mike-engel.com>
2017-09-09 14:18:07 -0600

    Update dependencies and do a slight refactor
    Additionally, fix tests!

--- server/actions/index.js
+++ server/actions/index.js
@@ -9,61 +9,47 @@ const sanitize = pick(allowedFields);
 const create = data => {
   const bookmark = sanitize(data);
 
-  return new Promise((resolve, reject) => {
-    knex("bookmarks")
-      .returning(allowedFields)
-      .insert(bookmark)
-      .then(newBookmarks => {
-        log.info({ bookmark: newBookmarks[0] }, "bookmark created");
+  return knex("bookmarks")
+    .returning(allowedFields)
+    .insert(bookmark)
+    .then(newBookmarks => {
+      log.info({ bookmark: newBookmarks[0] }, "bookmark created");
 
-        resolve(newBookmarks[0]);
-      })
-      .catch(reject);
-  });
+      return newBookmarks[0];
+    });
 };
 
 // destroy :: Query -> Promise
 const destroy = query => {
   const bookmarkQuery = sanitize(query);
 
-  return new Promise((resolve, reject) => {
-    knex("bookmarks")
-      .returning(allowedFields)
-      .where(bookmarkQuery)
-      .delete()
-      .then(deletedBookmarks => {
-        log.info({ bookmark: deletedBookmarks[0] }, "bookmark deleted");
+  return knex("bookmarks")
+    .returning(allowedFields)
+    .where(bookmarkQuery)
+    .delete()
+    .then(deletedBookmarks => {
+      log.info({ bookmark: deletedBookmarks[0] }, "bookmark deleted");
 
-        resolve(deletedBookmarks[0]);
-      })
-      .catch(reject);
-  });
+      return deletedBookmarks[0];
+    });
 };
 
 // find :: Query -> Promise
 const find = query => {
-  return new Promise((resolve, reject) => {
-    knex("bookmarks")
-      .returning(allowedFields)
-      .offset(query.offset || 0)
-      .limit(query.limit || null)
-      .where(omit(["limit", "offset"], query))
-      .orderBy("createdAt", "desc")
-      .then(resolve)
-      .catch(reject);
-  });
+  return knex("bookmarks")
+    .returning(allowedFields)
+    .offset(query.offset || 0)
+    .limit(query.limit || null)
+    .where(omit(["limit", "offset"], query))
+    .orderBy("createdAt", "desc");
 };
 
 // search :: Query -> Promise
 const search = searchTerm => {
-  return new Promise((resolve, reject) => {
-    knex("bookmarks")
-      .returning(allowedFields)
-      .whereRaw(`LOWER(title) LIKE '%${searchTerm.toLowerCase()}%'`)
-      .orderBy("createdAt", "desc")
-      .then(resolve)
-      .catch(reject);
-  });
+  return knex("bookmarks")
+    .returning(allowedFields)
+    .whereRaw(`LOWER(title) LIKE '%${searchTerm.toLowerCase()}%'`)
+    .orderBy("createdAt", "desc");
 };
 
 module.exports = {
--- server/server.test.js
+++ server/server.test.js
@@ -1,64 +1,83 @@
-const { knex } = require('config/db')
-const proxyquire = require('proxyquire').noPreserveCache()
+const { knex } = require("config/db");
+const proxyquire = require("proxyquire").noPreserveCache();
 
-const handlerFunc = (req, res, next) => {}
-const errorHandlerStub = sinon.stub().returns(handlerFunc)
-const requestHandlerStub = sinon.stub().returns(handlerFunc)
+const handlerFunc = (req, res, next) => next();
+const errorHandlerStub = sinon.stub().returns(handlerFunc);
+const requestHandlerStub = sinon.stub().returns(handlerFunc);
 const stubs = {
   raven: {
-    middleware: {
-      express: {
-        errorHandler: errorHandlerStub,
-        requestHandler: requestHandlerStub
-      }
-    }
+    config: sinon.stub().returnsThis(),
+    install: sinon.stub(),
+    errorHandler: errorHandlerStub,
+    requestHandler: requestHandlerStub
   }
-}
+};
 const bookmark = {
-  title: 'index test bookmark',
-  url: 'https://duckduckgo.com'
-}
-const bookmarks = Array.apply(null, { length: 26 }).map(() => bookmark)
+  title: "index test bookmark",
+  url: "https://duckduckgo.com"
+};
+const bookmarks = Array.apply(null, { length: 26 }).map(() => bookmark);
 
-before((done) => {
-  knex.batchInsert('bookmarks', bookmarks)
+before(done => {
+  knex
+    .batchInsert("bookmarks", bookmarks)
     .then(() => done())
-    .catch((err) => {
-      throw new Error('There was a problem seeding the test db: ', err)
-    })
-})
+    .catch(err => {
+      throw new Error("There was a problem seeding the test db: ", err);
+    });
+});
 
-after((done) => {
-  knex('bookmarks').truncate().then(() => done())
-})
+after(done => {
+  knex("bookmarks")
+    .truncate()
+    .then(() => done());
+});
 
-describe('server', () => {
+describe("server", () => {
   afterEach(() => {
-    errorHandlerStub.reset()
-    requestHandlerStub.reset()
-  })
+    stubs.raven.errorHandler.reset();
+    stubs.raven.requestHandler.reset();
+    stubs.raven.config.reset();
+    stubs.raven.install.reset();
 
-  it('should not use raven if no token exists', () => {
-    proxyquire('./index', Object.assign({}, stubs, {
-      config: {
-        ravenUrl: ''
-      }
-    }))
+    stubs.raven.config.returnsThis();
+    stubs.raven.errorHandler.returns(handlerFunc);
+    stubs.raven.requestHandler.returns(handlerFunc);
+  });
 
-    expect(errorHandlerStub.callCount).to.equal(0)
-    expect(requestHandlerStub.callCount).to.equal(0)
-  })
+  it("should not use raven if no token exists", () => {
+    proxyquire(
+      "./index",
+      Object.assign({}, stubs, {
+        config: {
+          ravenUrl: ""
+        }
+      })
+    );
 
-  it('should use raven if a token exists', () => {
-    proxyquire('./index', Object.assign({}, stubs, {
-      config: {
-        ravenUrl: '1234567890'
-      }
-    }))
+    expect(stubs.raven.config.calledOnce).to.be.false;
+    expect(stubs.raven.install.calledOnce).to.be.false;
+    expect(stubs.raven.errorHandler.calledOnce).to.be.false;
+    expect(stubs.raven.requestHandler.calledOnce).to.be.false;
+  });
 
-    expect(errorHandlerStub.callCount).to.equal(1)
-    expect(requestHandlerStub.callCount).to.equal(1)
-    expect(errorHandlerStub).to.have.been.calledWith('1234567890')
-    expect(requestHandlerStub).to.have.been.calledWith('1234567890')
-  })
-})
+  it("should use raven if a token exists", () => {
+    proxyquire(
+      "./index",
+      Object.assign({}, stubs, {
+        config: {
+          ravenUrl: "1234567890"
+        }
+      })
+    );
+
+    expect(stubs.raven.config.calledOnce).to.be.true;
+    expect(stubs.raven.install.calledOnce).to.be.true;
+    expect(stubs.raven.errorHandler.calledOnce).to.be.true;
+    expect(stubs.raven.requestHandler.calledOnce).to.be.true;
+    expect(stubs.raven.config.firstCall.args).to.deep.equal(["1234567890"]);
+    expect(stubs.raven.install.firstCall.args).to.deep.equal([]);
+    expect(stubs.raven.errorHandler.firstCall.args).to.deep.equal([]);
+    expect(stubs.raven.requestHandler.firstCall.args).to.deep.equal([]);
+  });
+});
